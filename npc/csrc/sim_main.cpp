385c09514fd619c788d3f1f787574352a020c06a
tracer-ysyx <tracer@ysyx.org>
2025-10-13 18:34:51 +0800

    > sim RTL ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-84-generic #84~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 14:29:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:34:51 up 1 day, 14:14,  1 user,  load average: 0.41, 0.24, 0.11

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -27,9 +27,9 @@ int main(int argc, char* argv[]) {
     top->trace(tfp, 99);
 
 #if WAVE_FST
-    tfp->open("build/log/wave.fst");
+    tfp->open("build/logs/wave.fst");
 #else
-    tfp->open("build/log/wave.vcd");
+    tfp->open("build/logs/wave.vcd");
 #endif
 
     int cycle = 50;
