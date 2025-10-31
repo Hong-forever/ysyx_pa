149cfff30cab64d9b2d95143f08bb22309717bf7
tracer-ysyx <tracer@ysyx.org>
2025-10-31 17:41:55 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:41:55 up 18:02,  1 user,  load average: 0.12, 0.45, 0.61

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -51,7 +51,7 @@ void engine_start() {
     uint32_t actual = eval_expr(expr_buf, &success);
 
     if(success && actual == expected) {
-        printf("success: %u\n", i);
+        /* printf("success: %u\n", i); */
         pass_num++;
     } else {
         printf("Error %u\nExpected: %u, Actua: %u, %s\n", i, expected, actual,expr_buf);
