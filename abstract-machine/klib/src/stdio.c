b14744f9d630327881a9b9ba45b6c191397afb9b
tracer-ysyx <tracer@ysyx.org>
2025-11-04 00:52:22 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:52:22 up 2 days,  9:55,  4 users,  load average: 0.49, 0.60, 0.50

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,8 +5,40 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char *print_int_to_buf(char *out, int num) {
+    char buffer[32];
+    int i = 0;
+    int is_neg = 0;
+    
+    if(num < 0) {
+        is_neg = 1;
+        num = -num;
+    }
+
+    if(num == 0) {
+        buffer[i++] = '0';
+    } else {
+        while(num > 0) {
+            buffer[i++] = '0' + (num % 10);
+            num /= 10;
+        }
+    }
+
+    if(is_neg) *out++ = '-';
+
+    for(int j=i-1; j>=0; j--) *out++ = buffer[j];
+
+    return out;
+}
+
+char *print_str_to_buf(char *out, char *str) {
+    while(*str) *out++ = *str++;
+
+    return out;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+    panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -14,7 +46,46 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+    va_list ap;
+    char *origin_out = out; 
+
+    va_start(ap, fmt);
+
+    while(*fmt) {
+        if(*fmt == '%') {
+            fmt++;
+            switch (*fmt) {
+                case 'd': {
+                    int num = va_arg(ap, int);
+                    out = print_int_to_buf(out, num);
+                    break;
+                }
+                case 's': {
+                    char *str = va_arg(ap, char *);
+                    out = print_str_to_buf(out, str);
+                    break;
+                }
+                case 'c': {
+                    int ch = va_arg(ap, int);
+                    *out++ = (char)ch;
+                    break;
+                }
+                default: {
+                    *out++ = '%';
+                    *out++ = *fmt;
+                    break;
+                }
+            }
+        } else {
+            *out++ = *fmt;
+        }
+        fmt++;
+    }
+
+    *out = '\0';
+    va_end(ap);
+
+    return out - origin_out;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
