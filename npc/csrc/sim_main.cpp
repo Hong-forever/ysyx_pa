9d03370fb62c7e940f17488fce48f7a763ca55fe
tracer-ysyx <tracer@ysyx.org>
2025-11-06 18:57:53 +0800

    > sim RTL ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:57:53 up 3 days, 12:50,  3 users,  load average: 0.54, 0.58, 0.49

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -22,6 +22,7 @@ static int mem[65536] = {0};
 int trap_flag = 0;
 
 extern "C" int pmem_read(int raddr) {
+    printf("0x%08x\n", mem[raddr>>2]);
     return mem[raddr>>2];
 }
 
@@ -40,6 +41,7 @@ static int tra_mask(int wmask) {
 
 extern "C" void pmem_write(int waddr, int wdata, int wmask) {
     mem[waddr >> 2] = (wdata & tra_mask(wmask)) | (mem[waddr >> 2] & tra_mask(wmask));
+    printf("0x%08x: 0x%08x data 0x%08x mask\n", waddr, wdata, wmask);
 }
 
 extern "C" void trap(int reg_data) {
