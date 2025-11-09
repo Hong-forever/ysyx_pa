10efcb71cbbaafdad161abd41623319cd39ae643
tracer-ysyx <tracer@ysyx.org>
2025-11-09 22:45:57 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:45:57 up 5 days, 7 min,  5 users,  load average: 0.53, 0.43, 0.52

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,7 +13,7 @@ int sprintf(char *out, const char *fmt, ...);
 
 int printf(const char *fmt, ...) {
     va_list ap;
-    char out[STRLEN];
+    char *out = NULL;
 
     va_start(ap, fmt);
     int ret = sprintf(out, fmt, ap);
