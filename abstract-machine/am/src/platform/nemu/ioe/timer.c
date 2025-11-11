111b7801a31536530b8b81ca349cf2494bf730f6
tracer-ysyx <tracer@ysyx.org>
2025-11-11 22:40:59 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:40:59 up 5 days,  3:13,  5 users,  load average: 0.26, 0.47, 0.61

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -8,7 +8,7 @@ void __am_timer_init() {
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
     uint32_t time_lo = inl(RTC_ADDR);
     uint32_t time_hi = inl(RTC_ADDR+ 4);
-    printf("hi: %d lo: %d\n, %d", time_hi, time_lo);
+    printf("hi: %d lo: %d\n", time_hi, time_lo);
 
     uptime->us = ((uint64_t)time_hi << 32) | (uint64_t)time_lo;
 }
