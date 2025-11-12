7a3153f9dcc7009c139076fd009490829e7c04ec
tracer-ysyx <tracer@ysyx.org>
2025-11-13 01:46:53 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:46:53 up  4:02,  5 users,  load average: 0.46, 0.17, 0.18

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -21,12 +21,16 @@ static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
+  uint64_t a;
+  printf("offset: %d\n", offset);
   if (!is_write && offset == 4) {
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
     printf("us: %ld\n", us);
+    a = us;
   }
+    printf("no offset us: %ld\n", a);
 }
 
 #ifndef CONFIG_TARGET_AM
