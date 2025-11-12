86d5f3756e1061a9b9022785cd1f4cd5dcc1e059
tracer-ysyx <tracer@ysyx.org>
2025-11-12 22:27:20 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:27:20 up 43 min,  5 users,  load average: 0.16, 0.18, 0.18

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,44 +7,33 @@
 
 #define STRLEN 1024
 
-char *print_int_to_buf(char *out, int num);
-char *print_uint_to_buf(char *out, unsigned int num);
-char *print_str_to_buf(char *out, char *str); 
-int sprintf(char *out, const char *fmt, ...);
-
-int printf(const char *fmt, ...) {
-    va_list ap;
-    char out[STRLEN] = {0};
-
-    va_start(ap, fmt);
-    int ret = sprintf(out, fmt, ap);
-    va_end(ap);
-
-    putstr(out);
-
-    return ret;
-}
+static char *print_int_to_buf(char *out, int num);
+static char *print_uint_to_buf(char *out, unsigned int num);
+static char *print_str_to_buf(char *out, char *str); 
+static char *print_hex_to_buf(char *out, unsigned int num, int uppercase);
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-    va_list ap;
     char *origin_out = out; 
-
-    va_start(ap, fmt);
-
-    while(*fmt) {
-        if(*fmt == '%') {
+    
+    while (*fmt) {
+        if (*fmt == '%') {
             fmt++;
+            
+            int field_width = 0;
+            
+            while (*fmt >= '0' && *fmt <= '9') {
+                field_width = field_width * 10 + (*fmt - '0');
+                fmt++;
+            }
+            
             switch (*fmt) {
                 case 'u': {
                     unsigned int num = va_arg(ap, unsigned int);
                     out = print_uint_to_buf(out, num);
                     break;
                 }
-                case 'd': {
+                case 'd': 
+                case 'i': {
                     int num = va_arg(ap, int);
                     out = print_int_to_buf(out, num);
                     break;
@@ -59,30 +48,150 @@ int sprintf(char *out, const char *fmt, ...) {
                     *out++ = (char)ch;
                     break;
                 }
+                case 'x': {
+                    unsigned int num = va_arg(ap, unsigned int);
+                    out = print_hex_to_buf(out, num, 0);
+                    break;
+                }
+                case 'X': {
+                    unsigned int num = va_arg(ap, unsigned int);
+                    out = print_hex_to_buf(out, num, 1);
+                    break;
+                }
+                case '%': {
+                    *out++ = '%';
+                    break;
+                }
                 default: {
                     *out++ = '%';
                     *out++ = *fmt;
                     break;
                 }
             }
+            fmt++;
         } else {
-            *out++ = *fmt;
+            *out++ = *fmt++;
         }
-        fmt++;
     }
-
+    
     *out = '\0';
-    va_end(ap);
-
     return out - origin_out;
 }
 
+int printf(const char *fmt, ...) {
+    va_list ap;
+    char out[STRLEN] = {0};
+
+    va_start(ap, fmt);
+    int ret = vsprintf(out, fmt, ap);
+    va_end(ap);
+
+    putstr(out);
+
+    return ret;
+}
+
+
+int sprintf(char *out, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = vsprintf(out, fmt, ap);
+    va_end(ap);
+    return ret;
+}
+
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = vsnprintf(out, n, fmt, ap);
+    va_end(ap);
+    return ret;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    if (n == 0) return 0;
+    
+    char *origin_out = out;
+    char buffer[32];
+    size_t remaining = n - 1; // 为null终止符保留空间
+    
+    while (*fmt && remaining > 0) {
+        if (*fmt == '%') {
+            fmt++;
+            
+            switch (*fmt) {
+                case 'u': {
+                    unsigned int num = va_arg(ap, unsigned int);
+                    char *temp = buffer;
+                    temp = print_uint_to_buf(temp, num);
+                    *temp = '\0';
+                    
+                    size_t len = temp - buffer;
+                    if (len > remaining) len = remaining;
+                    
+                    memcpy(out, buffer, len);
+                    out += len;
+                    remaining -= len;
+                    break;
+                }
+                case 'd': 
+                case 'i': {
+                    int num = va_arg(ap, int);
+                    char *temp = buffer;
+                    temp = print_int_to_buf(temp, num);
+                    *temp = '\0';
+                    
+                    size_t len = temp - buffer;
+                    if (len > remaining) len = remaining;
+                    
+                    memcpy(out, buffer, len);
+                    out += len;
+                    remaining -= len;
+                    break;
+                }
+                case 's': {
+                    char *str = va_arg(ap, char *);
+                    size_t len = strlen(str);
+                    if (len > remaining) len = remaining;
+                    
+                    memcpy(out, str, len);
+                    out += len;
+                    remaining -= len;
+                    break;
+                }
+                case 'c': {
+                    if (remaining > 0) {
+                        int ch = va_arg(ap, int);
+                        *out++ = (char)ch;
+                        remaining--;
+                    }
+                    break;
+                }
+                case '%': {
+                    if (remaining > 0) {
+                        *out++ = '%';
+                        remaining--;
+                    }
+                    break;
+                }
+                default: {
+                    if (remaining > 1) {
+                        *out++ = '%';
+                        *out++ = *fmt;
+                        remaining -= 2;
+                    }
+                    break;
+                }
+            }
+            fmt++;
+        } else {
+            *out++ = *fmt++;
+            remaining--;
+        }
+    }
+    
+    *out = '\0';
+    return out - origin_out;
 }
 
 char *print_uint_to_buf(char *out, unsigned int num) {
@@ -104,40 +213,43 @@ char *print_uint_to_buf(char *out, unsigned int num) {
 }
 
 char *print_int_to_buf(char *out, int num) {
-    char buffer[34];
-    int i = 0;
-    int is_neg = 0;
-
-    if(num == (-2147483648)) {
+    if (num == (-2147483647 - 1)) { // 更安全的INT_MIN检查
         const char *min_str = "-2147483648";
-
-        while(*min_str) *out++ = *min_str++;
+        while (*min_str) *out++ = *min_str++;
         return out;
     }
     
-    if(num < 0) {
-        is_neg = 1;
+    if (num < 0) {
+        *out++ = '-';
         num = -num;
     }
-
-    if(num == 0) {
-        buffer[i++] = '0';
-    } else {
-        while(num > 0) {
-            buffer[i++] = '0' + (num % 10);
-            num /= 10;
-        }
-    }
-
-    if(is_neg) *out++ = '-';
-
-    for(int j=i-1; j>=0; j--) *out++ = buffer[j];
-
-    return out;
+    
+    return print_uint_to_buf(out, (unsigned int)num);
 }
 
 char *print_str_to_buf(char *out, char *str) {
     while(*str) *out++ = *str++;
+
+    return out;
+}
+
+char *print_hex_to_buf(char *out, unsigned int num, int uppercase) {
+    char buffer[32];
+    int i = 0;
+    const char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+    if (num == 0) {
+        buffer[i++] = '0';
+    } else {
+        while (num > 0) {
+            buffer[i++] = digits[num % 16];
+            num /= 16;
+        }
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        *out++ = buffer[j];
+    }
 
     return out;
 }
