b57aa18a53afa183685872d752decbd491177681
tracer-ysyx <tracer@ysyx.org>
2025-11-13 03:59:39 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:59:39 up  6:15,  5 users,  load average: 0.87, 0.58, 0.47

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -55,10 +55,10 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
-    printf("->map, %x\n", addr);
   return map_read(addr, len, fetch_mmio_map(addr));
 }
 
 void mmio_write(paddr_t addr, int len, word_t data) {
+    printf("->map, %x\n", addr);
   map_write(addr, len, data, fetch_mmio_map(addr));
 }
