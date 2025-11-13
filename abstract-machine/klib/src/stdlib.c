3cd7958884832e4191a84033d94c7f93a660c685
tracer-ysyx <tracer@ysyx.org>
2025-11-13 20:41:32 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:41:32 up 12:59,  5 users,  load average: 0.57, 0.41, 0.36

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,39 +4,53 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+extern size_t _heap_start;
 
 int rand(void) {
-  // RAND_MAX assumed to be 32767
-  next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+    // RAND_MAX assumed to be 32767
+    next = next * 1103515245 + 12345;
+    return (unsigned int)(next/65536) % 32768;
 }
 
 void srand(unsigned int seed) {
-  next = seed;
+    next = seed;
 }
 
 int abs(int x) {
-  return (x < 0 ? -x : x);
+    return (x < 0 ? -x : x);
 }
 
 int atoi(const char* nptr) {
-  int x = 0;
-  while (*nptr == ' ') { nptr ++; }
-  while (*nptr >= '0' && *nptr <= '9') {
-    x = x * 10 + *nptr - '0';
-    nptr ++;
-  }
-  return x;
+    int x = 0;
+    while (*nptr == ' ') { nptr ++; }
+    while (*nptr >= '0' && *nptr <= '9') {
+        x = x * 10 + *nptr - '0';
+        nptr ++;
+    }
+    return x;
 }
 
 void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+    // On native, malloc() will be called during initializaion of C runtime.
+    // Therefore do not call panic() here, else it will yield a dead recursion:
+    //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+
+    static unsigned char *addr = NULL; 
+    if(addr == NULL) addr = (unsigned char *)_heap_start;
+
+    if(size == 0) return NULL;
+
+    size = (size + sizeof(void *) - 1) & ~(sizeof(void *) -1);
+
+    void *ret = addr;
+    addr += size;
+
+    return ret;
+
 #endif
-  return NULL;
+
+    return NULL;
 }
 
 void free(void *ptr) {
