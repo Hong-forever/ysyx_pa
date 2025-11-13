d19eb822ab877d13486e0c669597b0a1d51f6bb2
tracer-ysyx <tracer@ysyx.org>
2025-11-13 21:17:02 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:02 up 13:35,  5 users,  load average: 0.30, 0.35, 0.37

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,7 +4,6 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-extern char _heap_start;
 
 int rand(void) {
     // RAND_MAX assumed to be 32767
@@ -37,6 +36,8 @@ void *malloc(size_t size) {
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
 
     static unsigned char *addr = NULL; 
+    extern size_t _heap_start;
+    if(addr == NULL) addr = (unsigned char *)_heap_start;
     printf("_heap: %x\n", _heap_start);
     printf("addr: %x\n", addr);
 
