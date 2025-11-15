a53122e50650179ce7aec71a82cfbcb68d8b957b
tracer-ysyx <tracer@ysyx.org>
2025-11-15 17:31:22 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:31:22 up 1 day,  3:41,  5 users,  load average: 0.46, 0.30, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -155,8 +155,7 @@ void cpu_exec(uint64_t n) {
         case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
         case NEMU_END: case NEMU_ABORT:
-                           printf("aaa\n");
-                           mtrace_print(0x80000000, 4);
+                           mtrace_print(0x80000000, 200);
                            Log("nemu: %s at pc = " FMT_WORD,
                                    (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
                                     (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
