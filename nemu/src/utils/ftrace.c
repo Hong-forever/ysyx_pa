723d9489d70d3ac9bc5af7879924717a136300b4
tracer-ysyx <tracer@ysyx.org>
2025-11-16 16:45:02 +0800

    > compile NEMU ysyxhhh hhh Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:45:02 up 1 day, 19:39,  5 users,  load average: 0.18, 0.35, 0.33

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -160,7 +160,11 @@ void ftrace_exec(uint32_t pc, uint32_t dnpc, uint32_t rs1, uint32_t rd, uint32_t
         printf("0x%08x: ", pc);
         /* for(int i=0; i<call_depth; i++) printf("  "); */
         if(current_func && ret_to_func) {
-            printf("ret  [%s] (0x%08x)\n", ret_to_func, dnpc);
+            printf("ret [%s] (0x%08x)\n", ret_to_func, dnpc);
+        } else if(current_func) {
+            printf("ret [0x%08x]\n", dnpc);
+        } else {
+            printf("ret [0x%08x]\n", dnpc);
         }
     } else if((op == 1 || op == 2)) {
         const char *caller_name = find_function_name(pc);
@@ -175,6 +179,11 @@ void ftrace_exec(uint32_t pc, uint32_t dnpc, uint32_t rs1, uint32_t rd, uint32_t
         /* for(int i=0; i<call_depth-1; i++) printf("  "); */
         if(caller_name && callee_name) {
             printf("call [%s] (0x%08x)\n", callee_name, dnpc);
+        } else if(callee_name) {
+            printf("call [%s] (0x%08x)\n", callee_name, dnpc);
+        } else {
+            printf("call [0x%08x]\n", dnpc);
         }
+        
     }
 }
