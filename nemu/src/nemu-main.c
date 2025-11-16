0bfb998352640228f63e2d68b194c97af5d5292b
ysyx-hhh <3224318821@qq.com>
2025-11-16 17:06:06 +0800

    ftrace

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include <common.h>
+#include <ftrace.h>
 
 void init_monitor(int, char *[]);
 void am_init_monitor();
@@ -30,6 +31,8 @@ int main(int argc, char *argv[]) {
 
   /* Start engine. */
   engine_start();
+
+  cleanup_ftrace();
 
   return is_exit_status_bad();
 }
