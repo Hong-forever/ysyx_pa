81c8030a40ebb04fc99d3e15d17b4d133a4b3bf3
ysyx-hhh <3224318821@qq.com>
2025-11-20 00:29:38 +0800

    vga

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -72,6 +72,11 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
+    uint32_t *sync = vgactl_port_base + 1;
+    if(*sync) {
+        update_screen();
+        *sync = 0;
+    }
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
 }
