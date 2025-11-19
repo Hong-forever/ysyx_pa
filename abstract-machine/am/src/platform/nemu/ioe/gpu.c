efe89cd49f28ad0f406e9bff4aaf1f9313834cac
tracer-ysyx <tracer@ysyx.org>
2025-11-19 20:47:16 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:47:16 up 3 days, 13:17,  5 users,  load average: 0.15, 0.14, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,22 +4,30 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+    int i;
+    int w=(int)inw(VGACTL_ADDR+2); int h=(int)inw(VGACTL_ADDR);
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    for(i=0; i<w*h; i++) fb[i] = i;
+    outl(SYNC_ADDR, 1);
 }
 
+
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+    int w = (int)inw(VGACTL_ADDR+2); 
+    int h = (int)inw(VGACTL_ADDR);
+    *cfg = (AM_GPU_CONFIG_T) {
+        .present = true, .has_accel = false,
+        .width = w, .height = h,
+        .vmemsz = w * h
+    };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+    if (ctl->sync) {
+        outl(SYNC_ADDR, 1);
+    }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
+    status->ready = true;
 }
