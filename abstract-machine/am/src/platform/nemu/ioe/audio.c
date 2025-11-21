da207f4d97ecb4de7413878289deae92f0558adc
tracer-ysyx <tracer@ysyx.org>
2025-11-21 15:33:06 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:06 up 4 days, 10:32,  5 users,  load average: 0.68, 0.78, 1.03

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -49,7 +49,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl)
 
     uint8_t *src = (uint8_t *)ctl->buf.start;
     wpos = wpos % sbuf_size;
-    printf("Audio play 2 wlen: %u, wpos: %u\n", wlen, wpos);
+    printf("Audio play 2 wlen: %x, wpos: %x\n", wlen, wpos);
     uint8_t *dst = (uint8_t *)AUDIO_SBUF_ADDR;
 
     uint32_t first = sbuf_size - wpos;
@@ -59,7 +59,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl)
     if (remain)
         memcpy(dst, src + first, remain);
     wpos += wlen;
-    printf("Audio play after add: %u, wpos: %u\n", wlen, wpos);
+    printf("Audio play after add: %x, wpos: %x\n", wlen, wpos);
 
     if(!start) {
         outl(AUDIO_INIT_ADDR, 1);
