dc5cba7e403a621630d39c301d81edad1e747df4
tracer-ysyx <tracer@ysyx.org>
2025-11-21 21:15:09 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:15:09 up 4 days, 16:14,  5 users,  load average: 0.86, 0.38, 0.20

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -42,30 +42,38 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat)
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl)
 {
-    if (ctl->buf.start == NULL)
+    if (ctl->buf.start == NULL || ctl->buf.end == NULL || ctl->buf.start >= ctl->buf.end)
         return;
-    uint32_t wlen_all = ctl->buf.end - ctl->buf.start;
-    uint32_t wlen = wlen_all > 4096 ? 4096 : wlen_all;
-    while (inl(AUDIO_COUNT_ADDR) + wlen > sbuf_size) {
-        // printf("Audio wait... times: %d\r", ++times);
-    };
-
+    uint32_t wlen_all = (uint32_t)((uintptr_t)ctl->buf.end - (uintptr_t)ctl->buf.start);
+    if (wlen_all == 0) return;
     uint8_t *src = (uint8_t *)ctl->buf.start;
-    printf("src addr: %p, wlen: %d, wpos: %d\n", src, wlen, wpos);
     uint8_t *dst = (uint8_t *)AUDIO_SBUF_ADDR;
+    uint8_t *current_src = src;
 
-    uint32_t first = sbuf_size - wpos;
-    if (first > wlen) first = wlen;
-    for (int i = 0; i < first; i++) {
-        dst[wpos + i] = src[i];
-    }
-    uint32_t remain = wlen - first;
-    if (remain > 0)
-        for (int i = 0; i < remain; i++) {
-            dst[i] = src[first + i];
+    while (wlen_all > 0) {
+        uint32_t wlen = (wlen_all > 4096) ? 4096 : wlen_all;
+        while (inl(AUDIO_COUNT_ADDR) + wlen > sbuf_size) {
+            // 等待缓冲区空间
+        };
+
+        printf("src addr: %p, wlen: %d, wpos: %d\n", current_src, wlen, wpos);
+
+        uint32_t first = sbuf_size - wpos;
+        if (first > wlen) first = wlen;
+        for (int i = 0; i < first; i++) {
+            dst[wpos + i] = current_src[i];
         }
-    wpos = (wpos + wlen) % sbuf_size;
+        uint32_t remain = wlen - first;
+        if (remain > 0)
+            for (int i = 0; i < remain; i++) {
+                dst[i] = current_src[first + i];
+            }
+        wpos = (wpos + wlen) % sbuf_size;
 
-    uint32_t current_count = inl(AUDIO_COUNT_ADDR);
-    outl(AUDIO_COUNT_ADDR, current_count + wlen);
+        uint32_t current_count = inl(AUDIO_COUNT_ADDR);
+        outl(AUDIO_COUNT_ADDR, current_count + wlen);
+
+        current_src += wlen;
+        wlen_all -= wlen;
+    }
 }
