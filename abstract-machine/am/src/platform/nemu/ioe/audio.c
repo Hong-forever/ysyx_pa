3381221895e5cbb07681f45b0cb765c9a4b08b6d
tracer-ysyx <tracer@ysyx.org>
2025-11-21 19:26:54 +0800

    > compile NEMU ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:54 up 4 days, 14:26,  5 users,  load average: 0.36, 1.03, 1.27

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -11,7 +11,7 @@
 
 static uint32_t sbuf_size = 0;
 static uint32_t wpos = 0;
-static uint32_t times = 0;
+// static uint32_t times = 0;
 
 
 void __am_audio_init()
@@ -47,7 +47,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl)
     uint32_t wlen_all = ctl->buf.end - ctl->buf.start;
     uint32_t wlen = wlen_all > sbuf_size ? sbuf_size : wlen_all;
     while (inl(AUDIO_COUNT_ADDR) + wlen > sbuf_size) {
-        printf("Audio wait... times: %d\r", ++times);
+        // printf("Audio wait... times: %d\r", ++times);
     };
 
     uint8_t *src = (uint8_t *)ctl->buf.start;
