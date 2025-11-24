e366831de3abb07dc16aa6ddda496caf96a72cbb
ysyx-hhh <3224318821@qq.com>
2025-11-24 19:10:00 +0800

    read line

---
+++ npc/csrc/monitor/sdb/sdb.c
@@ -0,0 +1,68 @@
+#include "sdb.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void cpu_exec(uint64_t n);
+
+static char *rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(npc) ");
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
+
+static int cmd_c(char *args) {
+  // Continue the execution of the program
+  cpu_exec(-1);
+  return 0;
+}
+
+static int cmd_q(char *args) {
+  // Exit npc
+  return -1;
+}
+
+void sdb_mainloop()
+{
+    for (char *str; (str = rl_gets()) != NULL;) {
+        char *str_end = str + strlen(str);
+
+        /* extract the first token as the command */
+        char *cmd = strtok(str, " ");
+        if (cmd == NULL) {
+            continue;
+        }
+
+        /* treat the remaining string as the arguments,
+         * which may need further parsing
+         */
+        char *args = cmd + strlen(cmd) + 1;
+        if (args >= str_end) {
+            args = NULL;
+        }
+
+#ifdef CONFIG_DEVICE
+        extern void sdl_clear_event_queue();
+        sdl_clear_event_queue();
+#endif
+
+        // int i;
+        // for (i = 0; i < NR_CMD; i ++) {
+        //   if (strcmp(cmd, cmd_table[i].name) == 0) {
+        //     if (cmd_table[i].handler(args) < 0) { return; }
+        //     break;
+        //   }
+        // }
+        // if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    }
+}
