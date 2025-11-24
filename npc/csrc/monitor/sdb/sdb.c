19e86289d091f4b0cd63328504d34b74fc90268f
tracer-ysyx <tracer@ysyx.org>
2025-11-24 22:57:35 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:57:35 up  5:50,  6 users,  load average: 1.31, 1.18, 1.00

--- npc/csrc/monitor/sdb/sdb.c
+++ npc/csrc/monitor/sdb/sdb.c
@@ -1,39 +1,245 @@
 #include "sdb.h"
-#include <readline/readline.h>
-#include <readline/history.h>
 
 void cpu_exec(uint64_t n);
+static bool is_batch_mode = false;
 
-static char *rl_gets() {
-  static char *line_read = NULL;
+static int cmd_help(char *args);
+static int cmd_c(char *args);
+static int cmd_q(char *args);
+static int cmd_si(char *args);
+static int cmd_info(char *args);
+/*
+static int cmd_x(char *args);
+static int cmd_p(char *args);
+static int cmd_w(char *args);
+static int cmd_d(char *args);
+*/
 
-  if (line_read) {
-    free(line_read);
-    line_read = NULL;
-  }
+#define NR_CMD ARRLEN(cmd_table)
 
-  line_read = readline("(npc) ");
+static struct {
+    const char *name;
+    const char *description;
+    int (*handler)(char *);
+} cmd_table[] = {
+    { "help", "Display informations about all supported commands", cmd_help },
+    { "c", "Continue the execution of the program", cmd_c },
+    { "q", "Exit npc", cmd_q },
+    { "si", "Execute inst step", cmd_si },
+    { "info", "Print register or monitoring point information", cmd_info },
+    // { "x", "Find the value of the expression EXPR and use the result as the starting memory. The address is output in hexadecimal form as N consecutive 4-bytes", cmd_x },
+    // { "p", "Find the value of the expression EXPR", cmd_p },
+    // { "w", "Stop if EXPR changes", cmd_w },
+    // { "d", "Delete the monitor point with serial number N", cmd_d },
+};
 
-  if (line_read && *line_read) {
-    add_history(line_read);
-  }
+static char *rl_gets()
+{
+    static char *line_read = NULL;
 
-  return line_read;
+    if (line_read) {
+        free(line_read);
+        line_read = NULL;
+    }
+
+    line_read = readline("(npc) ");
+
+    if (line_read && *line_read) {
+        add_history(line_read);
+    }
+
+    return line_read;
 }
 
-static int cmd_c(char *args) {
-  // Continue the execution of the program
-  cpu_exec(-1);
-  return 0;
+static int cmd_help(char *args)
+{
+    /* extract the first argument */
+    char *arg = strtok(args, " ");
+    int i;
+
+    if (arg == NULL) {
+        /* no argument given */
+        for (i = 0; i < NR_CMD; i++) {
+            printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        }
+    } else {
+        for (i = 0; i < NR_CMD; i++) {
+            if (strcmp(arg, cmd_table[i].name) == 0) {
+                printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+                return 0;
+            }
+        }
+        printf("Unknown command '%s'\n", arg);
+    }
+    return 0;
 }
 
-static int cmd_q(char *args) {
-  // Exit npc
-  return -1;
+static int cmd_c(char *args)
+{
+    // Continue the execution of the program
+    cpu_exec(-1);
+    return 0;
+}
+
+static int cmd_q(char *args)
+{
+    // Exit npc
+    return -1;
+}
+
+static int cmd_si(char *args)
+{
+    char *arg = strtok(args, " ");
+    if (arg == NULL) {
+        cpu_exec(1);
+    } else {
+        for (char *p = arg; *p != '\0'; p++) {
+            if (!isdigit(*p)) {
+                printf("Error: need a number\n");
+                return 0;
+            }
+        }
+
+        int n = atoi(arg);
+        if (n <= 0) {
+            printf("Error: Must be a positive number\n");
+            return 0;
+        }
+
+        cpu_exec((uint64_t)n);
+    }
+
+    return 0;
+}
+
+static void info_cmd_print()
+{
+    printf("info <subcommand>\n");
+    printf(" r  - print Integer regs status\n");
+    printf(" w  - print watching point status\n");
+}
+
+static int cmd_info(char *args)
+{
+    char *arg = strtok(args, " ");
+    if (arg == NULL) {
+        info_cmd_print();
+    } else {
+        char cmd_char = arg[0];
+        if (arg[1] != '\0') {
+            printf("Unknown subcommand '%s'\n", arg);
+            info_cmd_print();
+            return 0;
+        } else {
+            switch (cmd_char) {
+            case 'r':
+                // isa_reg_display();
+                break;
+            case 'w':
+                // info_watchpoints();
+                break;
+            default:
+                printf("Unknown subcommand '%s'\n", arg);
+                info_cmd_print();
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
+
+/*
+static int cmd_x(char *args)
+{
+    char *arg = strtok(args, " ");
+    char *expr_arg = strtok(NULL, "");
+    bool success = false;
+    if(arg == NULL || expr_arg == NULL) {
+        printf("Error: Please input: x <N> <EXPR>\n");
+    } else {
+
+        int n = atoi(arg);
+        word_t expr_res = eval_expr(expr_arg, &success);
+        if(!success) {
+            printf("expr error\n");
+        }
+
+        for(int i=0; i<n; i++) {
+            printf("addr-0x%08x --> inst: %08x\n", expr_res+4*i, vaddr_read(expr_res+4*i, 4));
+        }
+    }
+    return 0;
+}
+
+static int cmd_p(char *args)
+{
+    if (args == NULL) {
+        printf("p <expr>\n");
+        printf("eg: p 1+2\n");
+        return 0;
+    }
+
+    bool success;
+    word_t result = eval_expr(args, &success);
+
+    if (success) {
+        printf("result: 0x%08x(%u)\n", result, result);
+    } else {
+        printf("Error to evaluate\n");
+    }
+
+    return success ? 0 : -1;
+}
+
+static int cmd_w(char *args)
+{
+    if (args == NULL) {
+        printf("Need expr to be a wp\n");
+        return 0;
+    }
+
+    set_watchpoint(args);
+    return 0;
+}
+
+static int cmd_d(char *args)
+{
+    char *arg = strtok(args, " ");
+    if (arg == NULL) {
+        printf("Error: Please input: d <N>\n");
+        return 0;
+    }
+
+    if(arg[0] < '0' || arg[0] > '9') {
+        printf("Error: should input d <N>\n");
+        printf("Error: Must be a num in [0,31]\n");
+        return 0;
+    }
+
+    int n = atoi(arg);
+    if (n < 0 || n >= 32) {
+        printf("Error: Please input: d <N>\n");
+        printf("Error: Must be a num in [0,31]\n");
+        return 0;
+    }
+
+    free_wp(n);
+    return 0;
+}
+*/
+
+void sdb_set_batch_mode()
+{
+    is_batch_mode = true;
 }
 
 void sdb_mainloop()
 {
+    if (is_batch_mode) {
+        cmd_c(NULL);
+        return;
+    }
     for (char *str; (str = rl_gets()) != NULL;) {
         char *str_end = str + strlen(str);
 
@@ -56,13 +262,13 @@ void sdb_mainloop()
         sdl_clear_event_queue();
 #endif
 
-        // int i;
-        // for (i = 0; i < NR_CMD; i ++) {
-        //   if (strcmp(cmd, cmd_table[i].name) == 0) {
-        //     if (cmd_table[i].handler(args) < 0) { return; }
-        //     break;
-        //   }
-        // }
-        // if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+        int i;
+        for (i = 0; i < NR_CMD; i ++) {
+          if (strcmp(cmd, cmd_table[i].name) == 0) {
+            if (cmd_table[i].handler(args) < 0) { return; }
+            break;
+          }
+        }
+        if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
     }
 }
