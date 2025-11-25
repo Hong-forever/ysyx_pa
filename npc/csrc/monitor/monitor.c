a1ee0eded0f6cf4ab626e7c80b18fead34fa3af5
tracer-ysyx <tracer@ysyx.org>
2025-11-25 20:06:19 +0800

    > sim RTL ysyx_25110270 HuangHaiHong Linux hhh-virtual-machine 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:06:19 up  9:29,  6 users,  load average: 0.76, 0.26, 0.09

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -18,6 +18,9 @@ static int parse_args(int argc, char *argv[]) {
     };
 
     int o;
+
+
+    
     while( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
         switch(o) {
             case 'b': sdb_set_batch_mode(); break;
